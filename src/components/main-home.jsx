4bf2702a1a7610b2e14b9946ed992266110a7adf
melanycpm1 <melanycatalanpabon@gmail.com>
2024-03-10 17:13:39 -0300

    carrousel

--- src/components/main-home.jsx
+++ src/components/main-home.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Hero from "./hero";
-
+import data from "../data/data";
 function MainHome() {
+
+    function Next() {
+        let nextImg = (count + 1) % images.length;
+        setCount(nextImg)
+    }
+    function previous() {
+        let previousImg = (count - 1) % images.length;
+        setCount(previousImg)
+    }
+    const [count, setCount] = useState(0);
+
+    let images = []
+
+    data.forEach(countries => {
+        images.push(countries.image)
+    });
+
     return (
         <>
-        <main>
-            <Hero></Hero>
-
-        </main>
+            <main className="h-1200">
+                <Hero></Hero>
+                <section className="relative top-16">
+                    <h2 className=" text-center my-24 text-4xl font-medium text-white font-serif">Popular MyTinerary</h2>
+                    <div className="flex justify-center">
+                        <img src={images[count]} className="w-2/4 h-96 object-cover"/>
+                    </div>
+                    <div className="flex justify-between relative bottom-40">
+                        <div>
+                            <span className="cursor-pointer" onClick={previous}>«—</span>
+                        </div>
+                        <div>
+                            <span className="cursor-pointer" onClick={Next}>—»</span>
+                        </div>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
